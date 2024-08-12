354268a3a998382bad34f74c5345923e9f93f17a
ahmedgomaaa <37199252+ahmedgomaaa@users.noreply.github.com>
2024-08-12 17:05:09 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    // Replace `your_command_here` with the command you want to run
+    exec('pwd', (error, stdout, stderr) => {
+        if (error) {
+            res.send(`<pre>Error: ${error.message}</pre>`);
+            return;
+        }
+        if (stderr) {
+            res.send(`<pre>stderr: ${stderr}</pre>`);
+            return;
+        }
+        res.send(`<pre>${stdout}</pre>`);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
